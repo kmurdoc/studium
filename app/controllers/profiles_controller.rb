6f496b51546aba3dc1fc3924505f88562e4aa3ee
Ryan Ke <rke@uncc.edu>
2018-11-08 05:14:33 +0000

    Complete Create and Show.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,22 @@
 class ProfilesController < ApplicationController
+    def show
+        @profile = Profile.find(params[:id])
+    end
+    
     def new
     end
     
     def create
-        render plain: params[:profile].inspect
+        @profile = Profile.new(profile_params)
+        
+        @profile.save
+        redirect_to @profile
+        
     end
 end
+
+private
+    def profile_params
+        params.require(:profile).permit(:firstName, :lastName, :classLevel, :major,
+                                            :course1, :course2, :course3, :course4, :course5)
+    end 
